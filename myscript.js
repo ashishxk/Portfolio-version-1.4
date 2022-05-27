fb7559c6d01218cdc36e97181c8ae3923107cd1e
ashy31 <56349542+ashy31@users.noreply.github.com>
2022-05-27 22:40:17 +0530

    commit

--- myscript.js
+++ myscript.js
@@ -1,10 +1,11 @@
 const showMenu = (toggleId, navId) =>{
   const toggle = document.getElementById(toggleId),
   nav = document.getElementById(navId)
-
+  
+  
   if(toggle && nav){
       toggle.addEventListener('click', ()=>{
-          nav.classList.toggle('show')
+          nav.classList.toggle('show-menu')
       })
   }
 }
@@ -12,11 +13,12 @@ showMenu('nav-toggle','nav-menu')
 
 
 
+
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
   const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
+  navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
